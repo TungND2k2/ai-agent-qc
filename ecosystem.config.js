8278c77e7ae8911e041e52c7bc59423c4dca0ffe
Nguyen Tung <tung.nd@x-or.cloud>
2025-06-19 15:41:46 +0700

    first commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps: [
     {
       name: 'dev:all',
-      script: '/root/.nvm/versions/node/v22.11.0/bin/node',
-      args: '/root/.nvm/versions/node/v22.11.0/lib/node_modules/npm/bin/npx-cli.js concurrently -n "BOT,BACKEND,PTR" -c "cyan,green,magenta" "npm run dev:bot" "npm run dev:backend" "npm run dev:puppeteer"',
+      script: './dev-all.sh',
+      interpreter: 'bash',
       env: {
         NODE_ENV: 'development',
       },
